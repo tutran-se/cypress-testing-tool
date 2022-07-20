77fde06f1157c9236ce06cb35434639e53727102
Tu Tran <trandinhtu.dev@gmail.com>
2022-07-20 10:19:12 -0400

    initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="mx-auto flex h-screen justify-center items-center">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
